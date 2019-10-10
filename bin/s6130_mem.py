54ea40ccccd7f48fa1bff632d0bb2279db7fa22f
radiolok <radiolok@yandex.ru>
2019-10-10 23:59:22 +0300

    S6130 Modules map

---
+++ bin/s6130_mem.py
@@ -0,0 +1,33 @@
+
+import sys
+
+
+ROM = {
+"ROM" :
+    [
+        {
+            "startAddr" : 0x0000,  "endAddr" : 0x07ff
+        }, 
+        {
+            "startAddr" : 0x0800, "endAddr" : 0x0fff
+        },
+        {
+            "startAddr" : 0x1000, "endAddr" : 0x17ff
+        },  
+        {
+            "startAddr" : 0x1800, "endAddr" : 0x1fff
+        },  
+        {
+            "startAddr" : 0x2000, "endAddr" : 0x27ff
+        }, 
+        {
+            "startAddr" : 0x2800, "endAddr" : 0x2fff
+        },  
+        {
+            "startAddr" : 0x3000, "endAddr" : 0x37ff
+        },
+        {
+            "startAddr" : 0x3800, "endAddr" : 0x3fff
+        }
+    ]            
+}
